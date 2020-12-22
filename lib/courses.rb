1f0320e188b999f1146c9ee84d54f937ff115678
Cara Zellner <carajeanine@gmail.com>
2020-12-22 18:51:00 +0000

    Automatically backed up by Learn

--- lib/courses.rb
+++ lib/courses.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses = doc.css("id-3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
   puts course.text.strip
 end
 
-id="id-3c239880-48cf-5ce7-84d0-578546d87586"
+# id="id-3c239880-48cf-5ce7-84d0-578546d87586"
